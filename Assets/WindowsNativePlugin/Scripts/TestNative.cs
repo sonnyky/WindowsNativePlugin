72ee1ef878e657830ccde5392f66cd72e1799a42
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2017-12-27 18:58:53 +0900

    Initial commit

---
+++ Assets/WindowsNativePlugin/Scripts/TestNative.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TestNative : MonoBehaviour {
+
+    [DllImport("XtionCapture", EntryPoint = "TestHello")]
+    public static extern IntPtr TestHello();
+
+    // Use this for initialization
+    void Start () {
+        IntPtr returnedChar = TestHello();
+        string returnedString = Marshal.PtrToStringAnsi(returnedChar);
+        Debug.Log("Returned : " + returnedString);
+        Marshal.FreeHGlobal(returnedChar);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
