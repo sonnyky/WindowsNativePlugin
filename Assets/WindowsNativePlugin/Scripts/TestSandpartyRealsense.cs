e1c075eeb11ddc07d855bed06c3f021f5d5a9f16
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2018-07-20 20:28:54 +0900

    New support for sandparty

---
+++ Assets/WindowsNativePlugin/Scripts/TestSandpartyRealsense.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TestSandpartyRealsense : MonoBehaviour
+{
+    private RealsenseSandparty realsenseInterface;
+    private HeightMap heightMap;
+
+    // Use this for initialization
+    void Start()
+    {
+        realsenseInterface = GameObject.Find("CameraInterface").GetComponent<RealsenseSandparty>();
+        heightMap = GameObject.Find("HeightMap").GetComponent<HeightMap>();
+    }
+   
+    public void InitDevice()
+    {
+        realsenseInterface.InitiateDevice();
+    }
+   
+    public void GetDepthData()
+    {
+        realsenseInterface.GetDepth();
+    }
+    public void GenerateHeightMap()
+    {
+        heightMap.StartGenerate();
+    }
+}
